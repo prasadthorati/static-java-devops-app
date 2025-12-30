4dc00832a4577e53e0a228e7fcbb106e28d7bae0
prasad <prasad@gmail.com>
2025-12-30 17:53:11 +0530

    demo_practice

---
+++ src/main/java/com/devops/staticapp/src/main/java/com/devops/staticapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.devops.staticapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Static Java DevOps App is running successfully!";
+    }
+}
